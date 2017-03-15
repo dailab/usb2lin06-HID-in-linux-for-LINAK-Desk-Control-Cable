17c914f1db63ee9d0fd51b1183d2d4e4e52d9bac
Thomas Geithner <thomas.geithner@dai-labor.de>
2017-03-15 17:05:06 +0100

    - fixed FIFO check on startup

--- desk-control/desk-control-speech.sh
+++ desk-control/desk-control-speech.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-/usr/bin/stdbuf -o0 /usr/bin/pocketsphinx_continuous \
-	-jsgf /usr/share/desk-control/desk.jsgf \
-	-dict /usr/share/desk-control/desk.dic \
-	-inmic yes -kws_threshold 1e-03 \
-	-adcdev /dev/audio1  -logfn /dev/null \
-	| /usr/bin/stdbuf -o0 /bin/egrep -v "^READY|^Listening" \
-	> /var/run/desk-control
+FIFO=/var/run/desk-control
 
+
+if [ ! -e $FIFO ]; then
+        echo "FIFO does not exit, creating it...";
+        mkfifo $FIFO
+elif [ ! -p $FIFO ]; then
+        echo "FIFO is a regular file, creating it...";
+        rm -f $FIFO
+        mkfifo $FIFO
+fi
+ 
+/usr/bin/stdbuf -o0 /usr/bin/pocketsphinx_continuous \
+        -jsgf /usr/share/desk-control/desk.jsgf \
+        -dict /usr/share/desk-control/desk.dic \
+        -inmic yes -kws_threshold 1e-03 \
+        -adcdev /dev/audio1  -logfn /dev/null \
+        | /usr/bin/stdbuf -o0 /bin/egrep -v "^READY|^Listening" \
+        > $FIFO
